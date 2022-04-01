a01d2fe15ed02a173676773cb1a0d1beb4d1f48c
ontaterbng <102777663+ontaterbng@users.noreply.github.com>
2022-04-01 11:16:12 +0700

    a

--- src/com/perpus/Main.java
+++ src/com/perpus/Main.java
@@ -2,7 +2,7 @@ package com.perpus;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tes 123124 ");
+        System.out.println("tes 677565 ");
         System.out.println("tes 234556 ");
         System.out.println("tes 99999 ");
     }
