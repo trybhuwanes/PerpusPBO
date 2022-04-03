dcfb7246ed6d97f928f9a4f03ceda5698f4a4d52
ajeng <ajengayukw@gmail.com>
2022-04-03 23:42:28 +0700

    listbuku

---
+++ src/com/perpus/listbuku.java
@@ -0,0 +1,18 @@
+package com.perpus;
+
+public class listbuku {
+    public static void main(String[] args) {
+        System.out.println("======= PERPUSTAKAAN PTI =======");
+        System.out.println("[01] The Pragmatic Programmer: Your Journey to Mastery");
+        System.out.println("[02] Structure and Interpretation of Computer Programs");
+        System.out.println("[03] Code Complete: A Practical Handbook of Software Construction");
+        System.out.println("[04] Clean Code: A Handbook of Agile Software Craftsmanship");
+        System.out.println("[05] Introduction to Algorithms");
+        System.out.println("[06] Peopleware: Productive Projects and Teams");
+        System.out.println("[07] Rapid Development: Taming Wild Software Schedules");
+        System.out.println("[08] The Self-Taught Programmer: The Definitive Guide to Programming Professionally");
+        System.out.println("[09] HTML and CSS: Design and Build Websites");
+        System.out.println("[10] Learning Web Design: A Beginner’s Guide to HTML, CSS, JavaScript, and Web Graphics");
+        System.out.println("---------------------");
+    }
+}
